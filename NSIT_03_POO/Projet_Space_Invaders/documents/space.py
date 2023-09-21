de1821b0b3cb562092a88a0e0ae7433f11d64a48
srt.trk <145556410+Dadarian@users.noreply.github.com>
2023-09-21 18:00:18 +0200

    Add files via upload
    fait en classe 21/09/2023

--- NSIT_03_POO/Projet_Space_Invaders/documents/space.py
+++ NSIT_03_POO/Projet_Space_Invaders/documents/space.py
@@ -3,4 +3,12 @@ import pygame  # necessaire pour charger les images et les sons
 
 class Joueur() : # classe pour créer le vaisseau du joueur
     def __init__(self) :
-        pass
+        self.sens = "0"
+        self.position = 400
+        self.image = pygame.image.load('vaisseau.png')
+        
+    def deplacer(self):
+        if self.sens == "gauche" and self.position>0 :
+            self.position -= 2
+        elif self.sens == "droite" and self.position<740:
+            self.position += 2
