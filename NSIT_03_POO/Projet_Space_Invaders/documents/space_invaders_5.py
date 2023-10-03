ce1b948bf00f45467cb37e3055eb89e07e1bc534
srt.trk <145556410+Dadarian@users.noreply.github.com>
2023-10-03 08:42:00 +0200

    Add files via upload

--- NSIT_03_POO/Projet_Space_Invaders/documents/space_invaders_5.py
+++ NSIT_03_POO/Projet_Space_Invaders/documents/space_invaders_5.py
@@ -1,67 +1,67 @@
-import pygame # importation de la librairie pygame
+import pygame
 import space
-import sys # pour fermer correctement l'application
+import sys
 
-# lancement des modules inclus dans pygame
-pygame.init() 
+pygame.init()
 
-# création d'une fenêtre de 800 par 600
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Space Invaders") 
-# chargement de l'image de fond
-fond = pygame.image.load('background.png')
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Space Invaders")
+fond = pygame.image.load('background2.png')
 
-# creation du joueur
 player = space.Joueur()
-# creation de la balle
 tir = space.Balle(player)
-tir.etat = "chargee"
-# creation des ennemis
+tir.etat = "attente"
+
 listeEnnemis = []
 for indice in range(space.Ennemi.NbEnnemis):
     vaisseau = space.Ennemi()
     listeEnnemis.append(vaisseau)
-    
-### BOUCLE DE JEU  ###
-running = True # variable pour laisser la fenêtre ouverte
 
-while running : # boucle infinie pour laisser la fenêtre ouverte
-    # dessin du fond
-    screen.blit(fond,(0,0))
+running = True
+while running:
+    screen.blit(fond, (0, 0))
 
-    ### Gestion des événements  ###
-    for event in pygame.event.get(): # parcours de tous les event pygame dans cette fenêtre
-        if event.type == pygame.QUIT : # si l'événement est le clic sur la fermeture de la fenêtre
-            running = False # running est sur False
-            sys.exit() # pour fermer correctement
-       
-       # gestion du clavier
-        if event.type == pygame.KEYDOWN : # si une touche a été tapée KEYUP quand on relache la touche
-            if event.key == pygame.K_LEFT : # si la touche est la fleche gauche
-                player.sens = "gauche" # on déplace le vaisseau de 1 pixel sur la gauche
-            if event.key == pygame.K_RIGHT : # si la touche est la fleche droite
-                player.sens = "droite" # on déplace le vaisseau de 1 pixel sur la gauche
-            if event.key == pygame.K_SPACE : # espace pour tirer
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.sens = "gauche"
+            if event.key == pygame.K_RIGHT:
+                player.sens = "droite"
+            if event.key == pygame.K_SPACE:
                 player.tirer()
                 tir.etat = "tiree"
 
-    ### Actualisation de la scene ###
-    # Gestions des collisions
     for ennemi in listeEnnemis:
         if tir.toucher(ennemi):
             ennemi.disparaitre()
             player.marquer()
-    print(f"Score = {player.score} points")
-    # placement des objets
-    # le joueur
+        if (
+    player.position < ennemi.depart + 40
+    and player.position + 40 > ennemi.depart
+    and player.vie > 0
+    and player.position + 40 > ennemi.hauteur
+    and player.position < ennemi.hauteur + 40
+):
+            player.perdre_vie()  # Le joueur perd une vie lorsqu'il entre en collision avec un ennemi
+
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Vies : {player.vie}", True, (255, 255, 255))
+    screen.blit(text, (10, 10))
+
+    if player.vie <= 0:
+        running = False
+
     player.deplacer()
-    screen.blit(tir.image,[tir.depart,tir.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur        
-    # la balle
+    screen.blit(tir.image, [tir.depart, tir.hauteur])
     tir.bouger()
-    screen.blit(player.image,[player.position,500]) # appel de la fonction qui dessine le vaisseau du joueur
-    # les ennemis
+    screen.blit(player.image, [player.position, 500])
+
     for ennemi in listeEnnemis:
         ennemi.avancer()
-        screen.blit(ennemi.image,[ennemi.depart, ennemi.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur
-        
-    pygame.display.update() # pour ajouter tout changement à l'écran
+        screen.blit(ennemi.image, [ennemi.depart, ennemi.hauteur])
+
+    pygame.display.update()
